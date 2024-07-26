51232a3184002ec6fb267796ffd62fe64c7e92a7
Sanjita Bhujel <bhujelsanjita7@gmail.com>
2024-07-26 10:43:30 +0545

    User controller added

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -4,6 +4,27 @@ const jwt = require("jsonwebtoken");
 
 const userController = {
     userLogin: (req,res)=>{
-        
+        if(
+            req.headers.authorization == null ||
+            req.headers.authorization == "" ||
+            req.headers.authorization == undefined
+        ){
+            return res.status.send({
+                message: "Unauthorised login",
+                success: false
+            });
+
+        }
+        if(
+            req.body.email ==  null ||
+            req.body.email == undefined ||
+            req.body.password == null ||
+            req.body.password == undefined
+        ){
+            return res.status(401).send({
+                message: "Please enter the valid details",
+                success: false
+            });
+        }
     }
 }
